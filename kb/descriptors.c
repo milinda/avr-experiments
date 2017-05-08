8af4c6aba7e0b080d311f8f7d456d0a11c8fe323
Milinda Pathirage <milinda.pathirage@gmail.com>
2017-05-07 20:33:20 -0400

    changes to descriptor

--- kb/descriptors.c
+++ kb/descriptors.c
@@ -11,34 +11,34 @@ const USB_Descriptor_HIDReport_Datatype_t PROGMEM KeyboardReport[] =
 	HID_RI_USAGE_PAGE(8, 0x01), /* Generic Desktop */
 	HID_RI_USAGE(8, 0x06), /* Keyboard */
 	HID_RI_COLLECTION(8, 0x01), /* Application */
-		HID_RI_USAGE_PAGE(8, 0x07), /* Key Codes */
-		HID_RI_USAGE_MINIMUM(8, 0xE0), /* Keyboard Left Control */
-		HID_RI_USAGE_MAXIMUM(8, 0xE7), /* Keyboard Right GUI */
-		HID_RI_LOGICAL_MINIMUM(8, 0x00),
-		HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-		HID_RI_REPORT_SIZE(8, 0x01),
-		HID_RI_REPORT_COUNT(8, 0x08),
-		HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
-		HID_RI_REPORT_COUNT(8, 0x01),
-		HID_RI_REPORT_SIZE(8, 0x08),
-		HID_RI_INPUT(8, HID_IOF_CONSTANT),
-		HID_RI_USAGE_PAGE(8, 0x08), /* LEDs */
-		HID_RI_USAGE_MINIMUM(8, 0x01), /* Num Lock */
-		HID_RI_USAGE_MAXIMUM(8, 0x05), /* Kana */
-		HID_RI_REPORT_COUNT(8, 0x05),
-		HID_RI_REPORT_SIZE(8, 0x01),
-		HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),
-		HID_RI_REPORT_COUNT(8, 0x01),
-		HID_RI_REPORT_SIZE(8, 0x03),
-		HID_RI_OUTPUT(8, HID_IOF_CONSTANT),
-		HID_RI_LOGICAL_MINIMUM(8, 0x00),
-		HID_RI_LOGICAL_MAXIMUM(8, 0x65),
-		HID_RI_USAGE_PAGE(8, 0x07), /* Keyboard */
-		HID_RI_USAGE_MINIMUM(8, 0x00), /* Reserved (no event indicated) */
-		HID_RI_USAGE_MAXIMUM(8, 0x65), /* Keyboard Application */
-		HID_RI_REPORT_COUNT(8, 0x06),
-		HID_RI_REPORT_SIZE(8, 0x08),
-		HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
+	HID_RI_USAGE_PAGE(8, 0x07), /* Key Codes */
+	HID_RI_USAGE_MINIMUM(8, 0xE0), /* Keyboard Left Control */
+	HID_RI_USAGE_MAXIMUM(8, 0xE7), /* Keyboard Right GUI */
+	HID_RI_LOGICAL_MINIMUM(8, 0x00),
+	HID_RI_LOGICAL_MAXIMUM(8, 0x01),
+	HID_RI_REPORT_SIZE(8, 0x01),
+	HID_RI_REPORT_COUNT(8, 0x08),
+	HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+	HID_RI_REPORT_COUNT(8, 0x01),
+	HID_RI_REPORT_SIZE(8, 0x08),
+	HID_RI_INPUT(8, HID_IOF_CONSTANT),
+	HID_RI_USAGE_PAGE(8, 0x08), /* LEDs */
+	HID_RI_USAGE_MINIMUM(8, 0x01), /* Num Lock */
+	HID_RI_USAGE_MAXIMUM(8, 0x05), /* Kana */
+	HID_RI_REPORT_COUNT(8, 0x05),
+	HID_RI_REPORT_SIZE(8, 0x01),
+	HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),
+	HID_RI_REPORT_COUNT(8, 0x01),
+	HID_RI_REPORT_SIZE(8, 0x03),
+	HID_RI_OUTPUT(8, HID_IOF_CONSTANT),
+	HID_RI_LOGICAL_MINIMUM(8, 0x00),
+	HID_RI_LOGICAL_MAXIMUM(8, 0x65),
+	HID_RI_USAGE_PAGE(8, 0x07), /* Keyboard */
+	HID_RI_USAGE_MINIMUM(8, 0x00), /* Reserved (no event indicated) */
+	HID_RI_USAGE_MAXIMUM(8, 0x65), /* Keyboard Application */
+	HID_RI_REPORT_COUNT(8, 0x06),
+	HID_RI_REPORT_SIZE(8, 0x08),
+	HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
 	HID_RI_END_COLLECTION(0),
 };
 
@@ -149,13 +149,13 @@ const USB_Descriptor_String_t PROGMEM LanguageString = USB_STRING_DESCRIPTOR_ARR
  *  form, and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
  *  Descriptor.
  */
-const USB_Descriptor_String_t PROGMEM ManufacturerString = USB_STRING_DESCRIPTOR(L"Dean Camera");
+const USB_Descriptor_String_t PROGMEM ManufacturerString = USB_STRING_DESCRIPTOR(L"Milinda Pathirage");
 
 /** Product descriptor string. This is a Unicode string containing the product's details in human readable form,
  *  and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
  *  Descriptor.
  */
-const USB_Descriptor_String_t PROGMEM ProductString = USB_STRING_DESCRIPTOR(L"LUFA Keyboard Demo");
+const USB_Descriptor_String_t PROGMEM ProductString = USB_STRING_DESCRIPTOR(L"LUFA Based Planck v0.1");
 
 /** This function is called by the library when in device mode, and must be overridden (see library "USB Descriptors"
  *  documentation) by the application code so that the address and size of a requested descriptor can be given
